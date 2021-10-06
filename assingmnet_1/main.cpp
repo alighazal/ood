99e4eb6d974376c29673b220c5ca0ecdb4c58eb7
alighazal <ali.mdghazal@gmail.com>
2021-10-06 02:54:18 +0200

    test: can test setting and getting vals

--- assingmnet_1/main.cpp
+++ assingmnet_1/main.cpp
@@ -6,7 +6,14 @@ using namespace std;
 
 int main() {
 
-    BitArray c(32);
+    BitArray c(32*4);
+    cout << c.Value(32*3+5) << endl;
+    c.Value(32*3+5, 1);
+    cout << c.Value(32*3+5) << endl;
+    
+    c.Value(32*3+5, 0);
+    cout << c.Value(32*3+5) << endl;
+
     return 0;
 
 }
