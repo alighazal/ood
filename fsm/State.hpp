5d32c87f0f88e2c730afb691c5489cb93f87c51c
alighazal <ali.mdghazal@gmail.com>
2021-10-29 23:52:05 +0200

    separate files

--- fsm/State.hpp
+++ fsm/State.hpp
@@ -14,6 +14,7 @@ class State{
     private:
         string name;
         vector<Action*> actions;
+        
     
     public:
         State(string name, vector<Action*> actions):name(name), actions(actions){}
